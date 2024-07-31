a15da63efdcb2df2d1e00967f2c850665d2808ad
javaMohit <mohit12.cdac@gmail.com>
2024-07-31 10:24:02 +0530

    new

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,13 +1,13 @@
 public class PrimeNumber {
     public static void main(String[] args) {
         int count = 0;
-        int num = 2;
+        int number = 2;
         while (count < 10) {
-            if (isPrime(num)) {
-                System.out.println(num);
+            if (isPrime(number)) {
+                System.out.println(number);
                 count++;
             }
-            num++;
+            number++;
         }
     }
 
